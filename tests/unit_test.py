39aa7d9dc20e63df6f3d10bc58cc54c4ec4c30a3
Jafar <205510122+Jafar891@users.noreply.github.com>
2026-04-04 19:19:31 +0200

    Unit test added (PyTest)

---
+++ tests/unit_test.py
@@ -0,0 +1,50 @@
+def test_api_key(api_key):
+    assert api_key == "MOCK_KEY1234"
+
+def test_channel_handle(channel_handle):
+    assert channel_handle == "CAR"
+
+
+def test_postgres_conn(mock_postgres_conn_vars):
+    conn = mock_postgres_conn_vars
+    assert conn.login == "mock_username"
+    assert conn.password == "mock_password"
+    assert conn.host == "mock_host"
+    assert conn.port == 1234
+    assert conn.schema == "mock_db_name"
+
+
+def test_dags_integrity(dagbag):
+    #1 Check for DAG import errors
+    assert dagbag.import_errors == {}, f"DAG import errors: {dagbag.import_errors}"
+    print("=================")
+    print(dagbag.import_errors)
+
+    #2 Check for expected DAGs
+    expeceted_dags = ["produce_json","update_db","data_quality_checks"]
+    loaded_dag_ids = list(dagbag.dags.keys())
+    print("=================")
+    print(dagbag.dags.keys())
+
+    for dag_id in expeceted_dags:
+        assert dag_id in loaded_dag_ids, f"DAG '{dag_id}' is missing from the DagBag"
+    
+    #3 Check for the total number of DAGs
+    assert dagbag.size() == 3
+    print("=================")
+    print(dagbag.size())
+
+    #4 Check for expected number of tasks in each DAG
+    expected_task_counts = {
+        "produce_json": 4,
+        "update_db": 2,
+        "data_quality_checks": 2
+    }
+    print("=================")
+    for dag_id, dag in dagbag.dags.items():
+        expected_count = expected_task_counts[dag_id]
+        actual_count = len(dag.tasks)
+        assert (
+            expected_count == actual_count
+        ), f"DAG '{dag_id}' has {actual_count} tasks, expected {expected_count}"
+        print(dag_id, len(dag.tasks))
